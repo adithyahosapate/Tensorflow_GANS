62943e5e703e6f53e3fde64adb4a57961b8394f6
adithyahosapate <adihosapate@gmail.com>
2017-09-12 00:26:50 +0530

    have to fix some bugs

--- test.py
+++ test.py
@@ -15,42 +15,40 @@ mnist = input_data.read_data_sets("MNIST_data/")
 #Initialize our GAN class from our helper function
 GAN=simpleGAN()
 
-discriminator=simpleGAN.discriminator()
 
-generator=simpleGAN.generator()
 
-graph=tf.Graph()
-with graph.as_default():
-	sess=tf.Session()
-	batch_size=50
-	z_dimensions=100
-	x_placeholder=tf.placeholder("float32",shape=[None,28,28,1],name="placeholder_x")
-	Gz=generator(batch_size,z_dimensions)
-	Dx=discriminator(x_placeholder)
-	g_loss=tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
+
+sess=tf.Session()
+batch_size=50
+z_dimensions=100
+x_placeholder=tf.placeholder("float32",shape=[None,28,28,1],name="placeholder_x")
+Gz=simpleGAN.generator(batch_size,z_dimensions)
+Dx=simpleGAN.discriminator(x_placeholder)
+Dg = simpleGAN.discriminator(Gz, reuse=True)
+
+g_loss=tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
 					logits=Dg,
 					labels=tf.ones_like(Dg)))
-	d_loss_real=tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
+d_loss_real=tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
 					logits=Dx,
 					labels=tf.fill([batch_size,1],0.9)))
-	d_loss_fake=tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
+d_loss_fake=tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
 					logits=Dg,
 					labels=tf.zeros_like(Dg)))
-	tvars = tf.trainable_variables()
 
-	d_vars = [var for var in tvars if 'd_' in var.name]
-	g_vars = [var for var in tvars if 'g_' in var.name]
+tvars = tf.trainable_variables()
 
-	with tf.variable_scope(tf.get_variable_scope(), reuse=False) as scope:
+d_vars = [var for var in tvars if 'd_' in var.name]
+g_vars = [var for var in tvars if 'g_' in var.name]
+
     #Next, we specify our two optimizers. In today’s era of deep learning, Adam seems to be the
     #best SGD optimizer as it utilizes adaptive learning rates and momentum. 
     #We call Adam's minimize function and also specify the variables that we want it to update.
-    	d_trainer_fake = tf.train.AdamOptimizer(0.0001).minimize(d_loss_fake, var_list=d_vars)
-    	d_trainer_real = tf.train.AdamOptimizer(0.0001).minimize(d_loss_real, var_list=d_vars)
+d_trainer_fake = tf.train.AdamOptimizer(0.0001).minimize(d_loss_fake, var_list=d_vars)
+d_trainer_real = tf.train.AdamOptimizer(0.0001).minimize(d_loss_real, var_list=d_vars)
 
     # Train the generator
     # Decreasing from 0.004 in GitHub version
-    	g_trainer = tf.train.AdamOptimizer(0.0001).minimize(g_loss, var_list=g_vars)
+g_trainer = tf.train.AdamOptimizer(0.0001).minimize(g_loss, var_list=g_vars)	
 
 
-    	
